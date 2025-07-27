8226b898df28089d3a7a99b81a7fafea4ac01df8
athishulleri01 <athishulleri@gmail.com>
2025-07-27 15:18:08 +0530

    fix: resolve bugs in view functions

--- core_apps/posts/views.py
+++ core_apps/posts/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-
-
-
-
-# blog/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
